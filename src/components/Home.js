e5a50d5032ed52b271f7ffc3d107ebcba02bf697
Anmol Topno <anmoltopno791@gmail.com>
2026-08-23 21:40:23 +0530

    Adding NoteItem in a Separate Note component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,6 @@
-import React, { useContext } from "react";
-import noteContext from "../context/notes/noteContext";
+import Notes from "./Notes";
 
 const Home = () => {
-  const context = useContext(noteContext);
-  const {notes, setNotes} = context;
   return (
     <div className="container my-3">
       <h1>Add a notes</h1>
@@ -46,12 +43,7 @@ const Home = () => {
           Submit
         </button>
       </form>
-      <div className="container my-3">
-        <h1>Your notes</h1>
-        {notes.map((note) => {
-          return note.title;
-        })}
-      </div>
+      <Notes/>
     </div>
   );
 };
