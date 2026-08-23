ab17165da36ccae6430de5535bda5f17c9361dd0
Anmol Topno <anmoltopno791@gmail.com>
2026-08-23 21:08:38 +0530

    useLocation Hook in React

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,22 +2,8 @@ import React, { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const s1 = {
-    name: "Anmol",
-    class: "5a",
-  };
-  const [state, setState] = useState(s1);
-  const update = () => {
-    setTimeout(() => {
-      setState({
-        name: "Nisha",
-        class: "10a",
-      });
-    }, 1000);
-  };
-
   return (
-    <noteContext.Provider value={{state, update}}>{props.children}</noteContext.Provider>
+    <noteContext.Provider value={{}}>{props.children}</noteContext.Provider>
   );
 };
 
