76accc7986f221baae98ce0b822364e944c47513
Anmol Topno <anmoltopno791@gmail.com>
2026-08-24 18:40:56 +0530

    Adding AddNote component to iNotebook

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -4,7 +4,7 @@ import noteContext from "./noteContext";
 const NoteState = (props) => {
   const notesInitial = [
     {
-      _id: "6a8b14dba00debec095f2bb8",
+      _id: "6a8b14dba00debec6095f2bb8",
       user: "6a848740ac1b635ef190f207",
       title: "New note",
       description: "Please access the playlist",
@@ -13,7 +13,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "6a8b150ea00debec095f2bb9",
+      _id: "6a8b150ea00debeic095f2bb9",
       user: "6a848740ac1b635ef190f207",
       title: "Github",
       description: "Please starting coding",
@@ -22,7 +22,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "6a8b14dba00debec095f2bb8",
+      _id: "6a8b14dba00deb5ec095f2bb8",
       user: "6a848740ac1b635ef190f207",
       title: "New note",
       description: "Please access the playlist",
@@ -31,7 +31,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "6a8b150ea00debec095f2bb9",
+      _id: "6a8b150ea00deb8ec095f2bb9",
       user: "6a848740ac1b635ef190f207",
       title: "Github",
       description: "Please starting coding",
@@ -40,7 +40,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "6a8b14dba00debec095f2bb8",
+      _id: "6a8b14dba00deb6ec095f2bb8",
       user: "6a848740ac1b635ef190f207",
       title: "New note",
       description: "Please access the playlist",
@@ -49,7 +49,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "6a8b150ea00debec095f2bb9",
+      _id: "6a8b150ea00deb2ec095f2bb9",
       user: "6a848740ac1b635ef190f207",
       title: "Github",
       description: "Please starting coding",
@@ -58,7 +58,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "6a8b14dba00debec095f2bb8",
+      _id: "6a8b14dba00debec0495f2bb8",
       user: "6a848740ac1b635ef190f207",
       title: "New note",
       description: "Please access the playlist",
@@ -67,7 +67,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "6a8b150ea00debec095f2bb9",
+      _id: "6a8b150ea00debec095fw2bb9",
       user: "6a848740ac1b635ef190f207",
       title: "Github",
       description: "Please starting coding",
@@ -78,8 +78,32 @@ const NoteState = (props) => {
   ];
   const [notes, setNotes] = useState(notesInitial);
 
+  // Add a Note
+  const addNote = (title, description, tag) => {
+    // TODO API Call
+    const note = {
+      _id: "6a8b14dba00debec09e5f2bb8",
+      user: "6a848740ac1b635ef190f207",
+      title: title,
+      description: description,
+      tag: tag,
+      date: "2026-08-23T15:42:19.359Z",
+      __v: 0,
+    };
+    setNotes(notes.concat(note));
+  };
+  // Delete a Note
+  const deleteNote = () => {
+
+  }
+
+  // Edit a Note
+  const editNote = () => {
+
+  }
+
   return (
-    <noteContext.Provider value={{ notes, setNotes }}>
+    <noteContext.Provider value={{ notes, setNotes, addNote, deleteNote, editNote  }}>
       {props.children}
     </noteContext.Provider>
   );
